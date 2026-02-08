87d136bcc84b32fd71abca30a66859f8cd0244ac
namqd201 <namqd201@gmail.com>
2026-02-08 12:45:49 +0700

    Add files via upload

---
+++ den.py
@@ -0,0 +1,16 @@
+# # h = 5  # chiều cao
+
+# # for i in range(h):
+# #      space = h - i - 1
+# #      star = 2 * i + 1
+# #      print(" " * space + "*" * star)
+# # h = 5
+# # for i in range (1, h + 1):
+# #     print("*" * i)
+# Cd = int(input("Chiều dài= "))
+# Cr = int(input("Chiều rộng= "))
+# C = (Cd + Cr) * 2
+# S = Cd * Cr
+# print(f"Chu vi là: ", C, "Diện tích là: ", S) 
+def hello():
+    print ("xin chào")
